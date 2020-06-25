621d5ab2a0e11a9f5ce84f848e1ffd953bceae5d
blobbyblobfish <aywang6@gmail.com>
2020-06-25 12:49:30 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,36 +3,30 @@ import React from 'react'
 const MenuBar = (props) => {
 
   /*
-
   The 'a' tags below are the menu items. Think about the way a menu 
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
   on a menu item? Do we need state in this component, and if not, how can
   this component be made aware of what is currently the active menu item?
-
   */
-
+    
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a className={props.status.profile} id="profile" onClick={props.handleClick}>
         <i className="user large icon" id="profile"/>
       </a>
-
-      <a className="item" id="photo">
+      <a className={props.status.photo} id="photo" onClick={props.handleClick}>
         <i className="photo large icon" id="photo"/>
       </a>
-
-      <a className="item" id="cocktail">
+      <a className={props.status.cocktail} id="cocktail" onClick={props.handleClick}>
         <i className="cocktail large icon" id="cocktail"/>
       </a>
-
-      <a className="item" id="pokemon"> 
+      <a className={props.status.pokemon} id="pokemon" onClick={props.handleClick}> 
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
-
 }
 
 export default MenuBar
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,28 +1,47 @@
 import React from 'react'
 import MenuBar from '../components/MenuBar.js'
-import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
+import { Profile, Photos, Cocktails, Pokemon } from '../components/Pages.js'
 
 class MainBox extends React.Component {
+  state = {
+    activeComponent: ""
+  }
 
+  handleClick = (evt) => {
+    this.setState({ activeComponent: evt.target.id })
+  }
+    
+  menuStatus = () => {
+    if (this.state.activeComponent === "") return { profile: "item", photo: "item", cocktail: "item", pokemon: "item" }
+    else if (this.state.activeComponent === "profile") return { profile: "item active", photo: "item", cocktail: "item", pokemon: "item" }
+    else if (this.state.activeComponent === "photo") return { profile: "item", photo: "item active", cocktail: "item", pokemon: "item" }
+    else if (this.state.activeComponent === "cocktail") return { profile: "item", photo: "item", cocktail: "item active", pokemon: "item" }
+    else if (this.state.activeComponent === "pokemon") return { profile: "item", photo: "item", cocktail: "item", pokemon: "item active" }
+  }
+  
 
   render() {
-
     /*
-
     Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
-
     */
-
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
+    const detailsToDisplay = {
+      "profile": <Profile />,
+      "photo": <Photos />,
+      "cocktail": <Cocktails />,
+      "pokemon": <Pokemon />
+    }
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar
+          status={this.menuStatus()}
+          handleClick={this.handleClick} 
+          
+        />
+        {detailsToDisplay[this.state.activeComponent]}
       </div>
     )
   }
-
 }
 
 export default MainBox
